d0f35a46293d1b7148a9c133c817c6395a599578
brut4luk3 <lucasreinert96@gmail.com>
2022-05-16 13:47:44 -0300

    Initial Commit

--- objects_and_places/urls.py
+++ objects_and_places/urls.py
@@ -6,4 +6,5 @@ app_name = 'objects_and_places'
 
 urlpatterns = [
     path('', rooms_views.index, name='index_rooms'),
+    path('places/<int:place_id>', rooms_views.detail, name='detail_rooms')
 ]
--- objects_and_places/views/rooms_views.py
+++ objects_and_places/views/rooms_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from ..models import Room
+from ..models import Room, Place
 
 
 def index(request):
@@ -14,3 +14,13 @@ def index(request):
     }
 
     return render(request, 'rooms/index.html', context=context)
+
+def detail(request, place_id):
+
+    places_list = get_object_or_404(Place, pk=place_id)
+
+    context = {
+        'places_list': places_list
+    }
+
+    return render(request, 'rooms/detail.html', context=context)
